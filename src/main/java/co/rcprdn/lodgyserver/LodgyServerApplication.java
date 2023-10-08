ab105ea0a7d594c60bfa8703314ea3707035a425
carnivalgoblin <rico.prodan@gmail.com>
2023-10-08 21:07:30 +0200

    Initial commit

---
+++ src/main/java/co/rcprdn/lodgyserver/LodgyServerApplication.java
@@ -0,0 +1,13 @@
+package co.rcprdn.lodgyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LodgyServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LodgyServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/co/rcprdn/lodgyserver/LodgyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.rcprdn.lodgyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LodgyServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
